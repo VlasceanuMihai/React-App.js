9a697187e4474fc3d4e5b79fad7689d704b217ef
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-02-01 21:23:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import './LogoComponent';
-import './Badge';
+import "./LogoComponent";
+import "./Badge";
 import LogoComponent from "./LogoComponent";
 import Badge from "./Badge";
 import Hello from "./Hello";
@@ -10,83 +10,81 @@ import CounterComponent from "./CounterComponent";
 const badges = [
   {
     firstName: "Mihai",
-    lastName: "Vlasceanu",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMJf32XCdIMPR005aLZbkk5TldBubjW2CfA&usqp=CAU"
+    lastName: "Vlasceanuuuu",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMJf32XCdIMPR005aLZbkk5TldBubjW2CfA&usqp=CAU",
   },
   {
     firstName: "Teodora",
     lastName: "Lavrig",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMJf32XCdIMPR005aLZbkk5TldBubjW2CfA&usqp=CAU"
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMJf32XCdIMPR005aLZbkk5TldBubjW2CfA&usqp=CAU",
   },
   {
     firstName: "Andreea",
     lastName: "Vlasceanu Antohi",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMJf32XCdIMPR005aLZbkk5TldBubjW2CfA&usqp=CAU"
-  }
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMJf32XCdIMPR005aLZbkk5TldBubjW2CfA&usqp=CAU",
+  },
 ];
 
 class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            counterValue: 0,
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      counterValue: 0,
+    };
+  }
 
-    render() {
-        const counterValueText = "First counter value " + this.state.counterValue;
-        return (
-            <div id = "container">
-              <Hello />
-              <LogoComponent />
-                <div>
-                    <button 
-                        onClick={(event) => {
-                            this.setState((state, props) => {
-                                return {
-                                    counterValue: state.counterValue - 1,
-                                };
-                            });
-                        }}
-                    >
-                        -
-                    </button>
+  render() {
+    const counterValueText = "First counter value " + this.state.counterValue;
+    return (
+      <div id="container">
+        <Hello />
+        <LogoComponent />
+        <div>
+          <button
+            onClick={(event) => {
+              this.setState((state, props) => {
+                return {
+                  counterValue: state.counterValue - 1,
+                };
+              });
+            }}
+          >
+            -
+          </button>
 
-                    <span>{ counterValueText }</span>
-                    <button
-                        onClick={(event) => {
-                            this.setState((state, props) => {
-                                return {
-                                    counterValue: state.counterValue + 1,
-                                };
-                            });
-                        }}
-                        >
-                            +
-                    </button>
-                </div>
+          <span>{counterValueText}</span>
+          <button
+            onClick={(event) => {
+              this.setState((state, props) => {
+                return {
+                  counterValue: state.counterValue + 1,
+                };
+              });
+            }}
+          >
+            +
+          </button>
+        </div>
 
-                <button
-                    onClick={(event) => {
-                        this.setState({ counterValue: 0});
-                    }}
-                    >
-                        Reset counter
-                </button>
-                
-                {badges.map(e => (
-                  <Badge
-                    firstName = {e.firstName}
-                    lastName = {e.lastName}
-                    avatar = {e.img}
-                    />
-                ))}
+        <button
+          onClick={(event) => {
+            this.setState({ counterValue: 0 });
+          }}
+        >
+          Reset counter
+        </button>
 
-                <CounterComponent />
-            </div>
+        {badges.map((e) => (
+          <Badge firstName={e.firstName} lastName={e.lastName} avatar={e.img} />
+        ))}
 
-        );
-    }
+        <CounterComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
